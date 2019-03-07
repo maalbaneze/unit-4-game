a18e4ab7f36fc37bd0dcae175548b3ce1798b473
Michael A <maalbaneze@aol.com>
2019-03-07 10:02:34 -0800

    Tweaked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,51 @@
 $(document).ready(function () {
 
+    var numberToGuess = Math.floor(Math.random() * 120) + 19;
+    // $("#random-number").text(numberToGuess);
+
+    var jewels = Math.floor(Math.random() * 12) + 1;
+    // $("#score-text").text(jewels);
+
     var counter = 0;
     var wins = 0;
     var losses = 0;
 
     function startGame() {
-        var numberToGuess = Math.floor(Math.random() * 120) + 19;
+        //var numberToGuess = Math.floor(Math.random() * 120) + 19;
         $("#random-number").text(numberToGuess);
 
-        var jewelValue = Math.floor(Math.random() * 12) + 1;
-        $("#score-text").text(jewelValue);
+        //var jewels = Math.floor(Math.random() * 12) + 1;
+        $("#score").text(jewels);
     }
+    startGame();
 
-    var jewels = [".ruby", ".amethyst", ".diamond", ".emerald"];
+    var crystals = [".ruby", ".amethyst", ".diamond", ".emerald"];
 
-    $(jewels).on('click', function () {
-        counter = counter + parseInt($(this).data('num'));
+    for (let i = 0; i < crystals.length; i++) {
 
-        $("#score-text").text(counter);
-        if (counter == numberToGuess) {
-            alert('You won!!!!');
-            wins++;
-            $("#wins").html(wins);
-            counter = 0;
-            $("#score-text").text(counter);
-            $("#score").html("");
-            resetnumberToGuess();
-            startGame();
-        } else if (counter > numberToGuess) {
-            alert('You lost!');
-            losses++;
-            $("#losses").html(losses);
-            counter = 0;
-            $("#score-text").text(counter);
-            $("#score").html("");
-            resetnumberToGuess();
-            startGame();
-        }
-    });
+        $(crystals[i]).on('click', function () {
+            counter = counter + parseInt($(this).data('num'));
+            console.log("crystal");
+            $("#score").text(counter);
+            if (counter == numberToGuess) {
+                alert('You won!!!!');
+                wins++;
+                $("#wins").html(wins);
+                counter = 0;
+                $("#score").text(counter);
+                // $("#score").html("");
+                resetnumberToGuess();
+                startGame();
+            } else if (counter > numberToGuess) {
+                alert('You lost!');
+                losses++;
+                $("#losses").html(losses);
+                counter = 0;
+                $("#score").text(counter);
+                // $("#score").html("");
+                resetnumberToGuess();
+                startGame();
+            }
+        });
+    }
 });
